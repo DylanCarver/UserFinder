1a8319ebfedfbd406705c7884ed99804eb5ac051
Dylan <dylc2770@gmail.com>
2018-09-05 19:19:37 +0200

    WIP - add docz, and add documentation of each component

---
+++ components/userInfo/demoUser.js
@@ -0,0 +1,10 @@
+const user = {
+  avatar_url: 'https://avatars0.githubusercontent.com/u/6746411?v=4',
+  name: 'Dylan Carver',
+  id: 12345,
+  public_repos: 99,
+  followers: 99,
+  following: 99,
+};
+
+export default user;
